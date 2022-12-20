8954259fda678e9e7b830e1e94720da4383afd0a
raulDeDevWorld <proyection009@gmail.com>
2022-12-20 09:00:28 -0400

    DC

--- context/Context.js
+++ context/Context.js
@@ -10,7 +10,7 @@ export function UserProvider({ children }) {
 	const [userDB, setUserDB] = useState('loading')
 	const [success, setSuccess] = useState(null)
 	const [image, setImage] = useState({})
-	const [numeration, setNumeration] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,43, 44])
+	const [numeration, setNumeration] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,43, 44, 45, 46])
 	const [pageOne, setPageOne] = useState(true);
 	const [pageTwo, setPageTwo] = useState(true);
 	const [pageThree, setPageThree] = useState(true);
